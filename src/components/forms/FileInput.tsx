8083b71bba052bfb835f5c220a49a729f820b258
yikl1004 <yikl1004@gmail.com>
2020-06-02 01:14:17 +0900

    file input develop

--- src/components/forms/FileInput.tsx
+++ src/components/forms/FileInput.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
-import { MutableRefObject, ChangeEvent } from 'react'
+import { jsx, css } from '@emotion/core'
+import { MutableRefObject, ChangeEvent, useState } from 'react'
 
 
 export interface IFileInputProps {
@@ -15,20 +15,27 @@ export interface IFileInputProps {
 }
 
 const FileInput = ({ register, name, accept = [], label = '파일업로드', ...rest }: IFileInputProps) => {
+    const [filename, setFilename] = useState('')
     const accepts = accept.reduce((acc, item, idx) => {
         return acc + (idx === 0 ? `.${item}` : `, .${item}`)
     }, '')
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const files = e.target.files
-        if ( files && files[0].size > (1 * 1024 * 1024 ) ) {
+        const files = e.target.files as FileList
+    
+        if ( files.length && files[0].size > (10 * 1024 * 1024 ) ) {
             alert('용량 제한')
             e.target.value = ''
+        } else {
+            if ( files.length ) {
+                setFilename(files[0].name)
+            }
         }
     }
 
     return (
-        <label>
-            <span>{ label }</span>
+        <label css={fileInput}>
+            <span className="fake-button">{ label }</span>
+            <span className="fake-input">{ filename }</span>
             <input 
                 accept={accepts} 
                 type="file" 
@@ -42,3 +49,38 @@ const FileInput = ({ register, name, accept = [], label = '파일업로드', ...
 };
 
 export default FileInput;
+
+
+const fileInput = css`
+    position: relative;
+    display: flex;
+    width: 280px;
+
+    input {
+        position: absolute;
+        display: block;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        top: 0;
+        left: 0;
+        cursor: pointer;
+    }
+    .fake-button {
+        display: inherit;
+        flex-shrink: 0;
+        flex-grow: 0;
+        background-color: #000;
+        padding: 5px 10px;
+        color: #fff;
+    }
+    .fake-input {
+        display: inherit;
+        box-sizing: border-box;
+        border: 1px solid #000;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        width: 100%;
+    }
+`
